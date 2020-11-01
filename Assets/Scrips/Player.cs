e075248595a1a02a64d93cb626ee23b8752d59e8
TomerRO <tomerrose141@gmail.com>
2020-11-01 16:48:15 +0200

    first push

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyWasPressed;
+    private float horizantalInput;
+    private Rigidbody rigidBodyComponent;
+    private bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space is pressed down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizantalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (jumpKeyWasPressed)
+        {
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rigidBodyComponent.velocity = new Vector3(horizantalInput, rigidBodyComponent.velocity.y, 0);
+    }
+
+    
+}
